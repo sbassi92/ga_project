60a589e44eceea1f462c4db28299f5413c4447b9
sbassi92 <sbassi@email.wm.edu>
2019-10-07 19:13:05 -0400

    adding learning module

---
+++ learn_express/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+app.use( (req, res, next) => {
+    res.setHeader("x-server-date", new Date());
+    return next();
+});
+
+app.get("/", (req, res, next) => {
+    return res.send("hello it's Jarvis");
+});
+
+app.get("/time", (req, res, next) => {
+    return res.send(new Date().toString());
+});
+
+app.get("/hello", (req, res, next) => {
+
+    if(!req.query.name)
+    {
+        return res.status(400).end();
+    }
+    else
+    {
+        return res.send( "hello " + req.query.name);
+    }
+});
+
+app.get("/user/:name", (req, res, next) => {
+
+        return res.send( "User " + req.params.name);
+});
+
+app.listen(3000);
